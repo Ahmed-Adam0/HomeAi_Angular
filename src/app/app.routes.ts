4110e7a642f28c74ee92d14b89ea7d2678b5792b
mayar <mayyarr200@gmail.com>
2026-05-23 16:28:36 +0200

    after r

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,45 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/home/pages/home/home')
+        .then(m => m.Home)
+  },
+
+  {
+    path: 'filter-sidebar',
+    loadComponent: () =>
+      import('./features/products/components/filter-sidebar/filter-sidebar')
+        .then(m => m.FilterSidebar)
+  },
+
+  {
+    path: 'Login',
+    loadComponent: () =>
+      import('./features/auth/pages/login/login')
+        .then(m => m.Login)
+  },
+  {
+    path: 'Register',
+    loadComponent: () =>
+      import('./features/auth/pages/register/register')
+        .then(m => m.Register)
+  },
+
+  {
+    path: 'profile',
+    loadComponent: () =>
+      import('./features/profile/pages/profile/profile')
+        .then(m => m.Profile)
+  },
+
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./shared/components/notfound/notfound')
+        .then(m => m.Notfound)
+  }
+];
---
+++ src/app/features/ai/pages/scan-room/scan-room.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scan-room',
+  imports: [],
+  templateUrl: './scan-room.html',
+  styleUrl: './scan-room.css',
+})
+export class ScanRoom {
+
+}
