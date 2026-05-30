61d4c5773e26ccdc9cc753056c1a80d0d90560c2
mayar <mayyarr200@gmail.com>
2026-05-30 21:46:49 +0200

    v3

--- src/app/features/vendor/pages/vendor-notifications/vendor-notifications.component.ts
+++ src/app/features/vendor/pages/vendor-notifications/vendor-notifications.component.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  signal,
+} from '@angular/core';
 import { NotificationsList } from '../../components';
+import { IVendorNotification } from '../../interfaces';
 
 @Component({
   selector: 'app-vendor-notifications',
@@ -7,5 +13,11 @@ import { NotificationsList } from '../../components';
   imports: [NotificationsList],
   templateUrl: './vendor-notifications.component.html',
   styleUrl: './vendor-notifications.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class VendorNotifications {}
+export class VendorNotifications {
+  readonly notifications = signal<IVendorNotification[]>([]);
+  readonly hasNotifications = computed(() => this.notifications().length > 0);
+
+  onMarkAsRead(id: string): void {}
+}
--- src/app/features/vendor/pages/vendor-order-details/vendor-order-details.component.ts
+++ src/app/features/vendor/pages/vendor-order-details/vendor-order-details.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { OrderStatusBadge } from '../../components';
+import { IVendorOrder } from '../../interfaces';
 
 @Component({
   selector: 'app-vendor-order-details',
   standalone: true,
-  imports: [OrderStatusBadge],
+  imports: [CommonModule, OrderStatusBadge],
   templateUrl: './vendor-order-details.component.html',
   styleUrl: './vendor-order-details.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class VendorOrderDetails {}
+export class VendorOrderDetails {
+  readonly order = signal<IVendorOrder | null>(null);
+
+  protected readonly skeletonItems = Array(3);
+}
