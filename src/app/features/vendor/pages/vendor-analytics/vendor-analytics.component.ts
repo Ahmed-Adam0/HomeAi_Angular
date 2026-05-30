7fb03555f6cda5eb310e57859a6c6e174f9e95fd
mayar <mayyarr200@gmail.com>
2026-05-30 20:11:31 +0200

    v2

--- src/app/features/vendor/pages/vendor-analytics/vendor-analytics.component.ts
+++ src/app/features/vendor/pages/vendor-analytics/vendor-analytics.component.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  signal,
+} from '@angular/core';
 import { AnalyticsCards } from '../../components';
+import { IVendorAnalytics } from '../../interfaces';
 
 @Component({
   selector: 'app-vendor-analytics',
@@ -7,5 +13,16 @@ import { AnalyticsCards } from '../../components';
   imports: [AnalyticsCards],
   templateUrl: './vendor-analytics.component.html',
   styleUrl: './vendor-analytics.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class VendorAnalytics {}
+export class VendorAnalytics {
+  readonly analytics = signal<IVendorAnalytics | null>(null);
+
+  readonly isAnalyticsLoading = computed(() => this.analytics() === null);
+
+  readonly kpiSkeletonSlots = [0, 1, 2, 3] as const;
+
+  readonly insightsPlaceholderSlots = [0, 1, 2] as const;
+
+  readonly productsPlaceholderSlots = [0, 1, 2, 3, 4] as const;
+}
